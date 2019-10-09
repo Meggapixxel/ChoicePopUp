cf631662b3aa6e4b566d0df8262994918981c69e
Meggapixxel <zhydenkodeveloper@gmail.com>
2019-10-09 11:09:24 +0300

    initial commit

--- Package.swift
+++ Package.swift
@@ -5,24 +5,19 @@ import PackageDescription
 
 let package = Package(
     name: "ChoicePopUp",
+    platforms: [
+        .iOS(.v11),
+    ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "ChoicePopUp",
-            targets: ["ChoicePopUp"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            targets: ["ChoicePopUp"]
+        )
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ChoicePopUp",
-            dependencies: []),
-        .testTarget(
-            name: "ChoicePopUpTests",
-            dependencies: ["ChoicePopUp"]),
+            dependencies: []
+        )
     ]
 )
--- Sources/ChoicePopUp/ChoicePopUp.swift
+++ Sources/ChoicePopUp/ChoicePopUp.swift
@@ -1,3 +1,99 @@
-struct ChoicePopUp {
-    var text = "Hello, World!"
+import UIKit
+
+public class ChoiceViewController<Element: P_ChoiceItem>: UITableViewController, UIPopoverPresentationControllerDelegate {
+    
+    typealias SelectionHandler = (Element) -> ()
+    
+    public enum Selection {
+        
+        case single((Element?) -> ())
+        case multiple(([Element]) -> ())
+        
+    }
+    
+    private var elements: [Element]
+    private let selection: Selection
+    private let completion: (() -> ())?
+    
+    public init(
+        preferredContentSize: CGSize = CGSize(width: 300, height: 200),
+        sourceView: UIView,
+        popupDisplayItems: [Element],
+        selection: Selection,
+        completion: (() -> ())? = nil
+    ) {
+        self.elements = popupDisplayItems
+        self.selection = selection
+        self.completion = completion
+        
+        super.init(style: .plain)
+        
+        self.preferredContentSize = preferredContentSize
+        self.modalPresentationStyle = .popover
+        let presentationController = self.presentationController as! UIPopoverPresentationController
+        presentationController.delegate = self//AlwaysPresentAsPopover.shared
+        presentationController.sourceView = sourceView
+        presentationController.sourceRect = sourceView.bounds
+        presentationController.permittedArrowDirections = [.down, .up]
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override public func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        guard isBeingDismissed else { return }
+        switch self.selection {
+        case .single(let completion): completion(elements.first { $0.isChoiceItemSelected } )
+        case .multiple(let completion): completion(elements.filter { $0.isChoiceItemSelected } )
+        }
+        completion?()
+    }
+    
+    // MARK: - UITableViewDataSource
+    override public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return elements.count
+    }
+    
+    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let element = elements[indexPath.row]
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = element.choiceItemDisplayValue
+        cell.accessoryType = element.isChoiceItemSelected ? .checkmark : .none
+        return cell
+    }
+    
+    // MARK: - UITableViewDelegate
+    override public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let common = {
+            self.elements[indexPath.row].isChoiceItemSelected.toggle()
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        }
+        switch selection {
+        case .single(_):
+            if let index = elements.firstIndex(where: { $0.isChoiceItemSelected } ) {
+                elements[index].isChoiceItemSelected = false
+                tableView.reloadRows(at: [IndexPath(row: index, section: indexPath.section)], with: .automatic)
+            }
+            common()
+            dismiss(animated: true)
+        case .multiple(_):
+            common()
+        }
+    }
+    
+    // MARK: - UIPopoverPresentationControllerDelegate
+    public func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
+        return .none
+    }
+    
+}
+
+public protocol P_ChoiceItem {
+    
+    var choiceItemDisplayValue: String { get }
+    var isChoiceItemSelected: Bool { get set }
+    
 }
